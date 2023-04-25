3dc38d3d063325e0c0277b5df82b4338db68a0a4
zwonlala <s26788761@naver.com>
2023-04-25 21:23:44 +0900

    feat: run lighthouse() and save data as JSON file

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] });
 
 const options = {
   logLevel: "info",
-  output: "html",
+  output: "json",
   onlyCategories: ["performance"],
   port: chrome.port,
 };
 const runnerResult = await lighthouse("https://local.miricanvas.com/", options);
 
 // `.report` is the HTML report as a string
-const reportHtml = runnerResult.report;
-fs.writeFileSync("lhreport.html", reportHtml);
+const reportJson = runnerResult.report;
+fs.writeFileSync("lhreport.json", reportJson);
 
 // `.lhr` is the Lighthouse Result as a JS object
 console.log("Report is done for", runnerResult.lhr.finalDisplayedUrl);
