c40dd23706fbcd96757ae99f1558d268205b3dd2
zwonlala <s26788761@naver.com>
2023-04-27 19:18:37 +0900

    feat: save report file at current Date directory as current Time file name

--- index.js
+++ index.js
@@ -15,8 +15,26 @@ const runnerResult = await lighthouse("https://local.miricanvas.com/", options);
 
 // `.report` is the HTML report as a string
 const reportJson = runnerResult.report;
-fs.writeFileSync("lhreport.json", reportJson);
 
+const currentDate = new Date();
+const dateOption = {
+  year: "2-digit",
+  month: "2-digit",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+};
+// "YY/MM/DD HH:MM" format
+const dateString = Intl.DateTimeFormat("ja", dateOption).format(currentDate);
+const [YYMMDD, HHMM] = dateString.split(" ");
+const dir = `./${YYMMDD.replace(/\//g, "")}`;
+const fileDatePrefix = HHMM.replace(":", "");
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+}
+
+fs.writeFileSync(`${dir}/${fileDatePrefix}_report.json`, reportJson);
 // `.lhr` is the Lighthouse Result as a JS object
 console.log("Report is done for", runnerResult.lhr.finalDisplayedUrl);
 console.log(
